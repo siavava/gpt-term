4d5bc6059e10d93b7fe49bf6be850df60f46e269
siavava <amittaijoel@outlook.com>
2023-12-19 05:45:46 -0500

    trim

--- src/main.py
+++ src/main.py
@@ -1,27 +1,15 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+from network import GPTLanguageModel, load_model, torch, device, TextProcessor
 
-from fastapi import FastAPI
+def main():
+  model: GPTLanguageModel = load_model("checkpoints/transfusion.pth")
+  response = None
+  while query := input("QUERY? "):
+    if query == "-": query = response or "Empty query"
+    context = torch.tensor( [TextProcessor.encode(query)], dtype=torch.long, device=device)
+    generated = model.generate(context, count=150)
+    response = TextProcessor.decode(generated[0].tolist())
+    print(f"\nRESPONSE:\n{response}\n\n")
 
-from network import GPTLanguageModel, load_model, encode, decode, torch, device
-
-app = FastAPI()
-model = load_model("network/models/transfusion.pth")
-
-@app.get("/")
-async def root():
-  return {"message": "Hello World"}
-
-@app.get("/api/{query}")
-async def api(query: str):
-
-  # create torch tensor with query as 2D array
-  context = torch.tensor( [encode(query)], dtype=torch.long, device=device)
-  
-  # generate from context
-  generated = model.generate(context, max_new_tokens=150)
-  
-  # decode generated response
-  response = decode(generated[0].tolist())
-
-  return { "query": query, "response": response }
+if __name__ == "__main__":
+  main()
---
+++ src/network.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+import torch; import torch.nn as nn; import torch.nn.functional as F
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+import yaml
+with open('config.yml', 'r') as f: globals().update(yaml.safe_load(f))
+
+class TextProcessor:
+  with open('data/input.txt', 'r', encoding='utf-8') as f:
+    chars = sorted(list(set(f.read())));            vocab_size = len(chars)
+    stoi = { ch:i for i,ch in enumerate(chars) };   itos = { i:ch for i,ch in enumerate(chars) }
+
+  @classmethod
+  def encode(cls, s): return [cls.stoi[c] for c in s]
+  
+  @classmethod
+  def decode(cls, l): return ''.join([cls.itos[i] for i in l])
+
+class Head(nn.Module):
+  """One self-attention head."""
+  def __init__(self, head_size):
+    super().__init__()
+    self.key = nn.Linear(embeddings_size, head_size, bias=False)
+    self.query = nn.Linear(embeddings_size, head_size, bias=False)
+    self.value = nn.Linear(embeddings_size, head_size, bias=False)
+    self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+    self.dropout = nn.Dropout(dropout)
+
+  def forward(self, x):
+    B,T,C = x.shape; k = self.key(x)
+    weights = self.query(x) @ k.transpose(-2, -1) * k.shape[-1] ** (-0.5)
+    weights = F.softmax(weights.masked_fill(self.tril[:T, :T] == 0, float('-inf')), dim=-1)
+    return self.dropout(weights) @ self.value(x)
+
+class MultiHeadAttention(nn.Module):
+  """ multiple heads of self-attention in parallel"""
+  def __init__(self, num_heads, head_size):
+    super().__init__()
+    self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+    self.proj = nn.Linear(head_size * num_heads, embeddings_size)
+    self.dropout = nn.Dropout(dropout)
+
+  def forward(self, x):
+    return self.dropout(self.proj(torch.cat([h(x) for h in self.heads], dim=-1)))
+
+class FeedFoward(nn.Module):
+  """Feed-forward network with ReLU activation and dropout."""
+  def __init__(self, embeddings_size):
+    super().__init__()
+    self.network = nn.Sequential(
+      nn.Linear(embeddings_size, 4 * embeddings_size), nn.ReLU(),
+      nn.Linear(4 * embeddings_size, embeddings_size), nn.Dropout(dropout),
+    )
+
+  def forward(self, x): return self.network(x)
+
+class Block(nn.Module):
+  """One transformer block. (self-attention, layer normalization, feed-forward, and residual connections)"""
+  def __init__(self, embeddings_size, head_count):
+    super().__init__()
+    head_size = embeddings_size // head_count
+    self.sa = MultiHeadAttention(head_count, head_size);  self.ffwd = FeedFoward(embeddings_size)
+    self.ln1 = nn.LayerNorm(embeddings_size);             self.ln2 = nn.LayerNorm(embeddings_size)
+
+  def forward(self, x):
+    x += self.sa(self.ln1(x)); x += self.ffwd(self.ln2(x)); return x
+
+class GPTLanguageModel(nn.Module):
+  """GPT language model with a single embedding layer, n layers, and a linear output layer."""
+  def __init__(self):
+    super().__init__()
+    self.token_embedding_table = nn.Embedding(TextProcessor.vocab_size, embeddings_size)
+    self.position_embedding_table = nn.Embedding(block_size, embeddings_size)
+    self.blocks = nn.Sequential(*[Block(embeddings_size, head_count=head_count) for _ in range(n_layer)])
+    self.ln_f = nn.LayerNorm(embeddings_size)
+    self.lm_head = nn.Linear(embeddings_size, TextProcessor.vocab_size)
+    self.apply(self._init_weights)
+
+  def _init_weights(self, module):
+    if isinstance(module, nn.Linear):
+      torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+      if module.bias is not None: torch.nn.init.zeros_(module.bias)
+    elif isinstance(module, nn.Embedding):
+      torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+  def forward(self, index, targets=None):
+    """forward pass. returns logits for next token, and optionally the computed loss."""
+    B, T = index.shape
+    token_embeddings = self.token_embedding_table(index)
+    positional_embeddings = self.position_embedding_table(torch.arange(T, device=device))
+    logits = self.lm_head(self.ln_f(self.blocks(token_embeddings + positional_embeddings)))
+    return logits
+
+  def generate(self, index, count):
+    """Generate up to *count* new tokens given a starting index."""
+    for _ in range(count):
+      index_next = torch.multinomial(F.softmax(self(index[:, -block_size:])[:, -1, :], dim=-1), num_samples=1)
+      index = torch.cat((index, index_next), dim=1)
+    return index
+
+def load_model(path="checkpoints/transfusion.pth") -> GPTLanguageModel:
+  model = GPTLanguageModel()
+  model.load_state_dict(torch.load(path, map_location=device))
+  return model.to(device)
